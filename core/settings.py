be9d401ad65f2e1179ff013d8353923ce9267c77
Marco Alber <m.alber@onlinehome.de>
2025-11-16 21:00:53 +0100

    fixed settings

--- core/settings.py
+++ core/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = "/Quarco/Desktop/backend/Coderr/staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
